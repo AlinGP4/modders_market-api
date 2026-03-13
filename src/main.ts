1155629d4c79ef3e7a41fde22992294916e48a39
AlinGP4 <alingabrielp4@gmail.com>
2026-03-13 16:44:15 +0100

    update origin

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:4200', 'http://localhost:3000'],
+    origin: ['http://localhost:4200', 'http://localhost:3000','https://www.modders-market.com','https://modders-market.com'],
     credentials: true
   });
 
